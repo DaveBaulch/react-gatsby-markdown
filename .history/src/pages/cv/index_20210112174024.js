e0416e470fa04a45e5f3ed9a4200f720953ae0d6
Dave Baulch <davebaulch@yahoo.co.uk>
2021-01-12 17:48:33 +0000

    cv page refactored

---
+++ .history/src/pages/cv/index_20210112174024.js
@@ -0,0 +1,68 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { graphql } from "gatsby"
+import Layout from "../../components/Layout"
+import CvArticle from "../../components/CvArticle"
+
+const md = ({ data }) => {
+  const { frontmatter, html } = data.allMarkdownRemark.edges[0].node
+
+  return (
+    <>
+      <h1 className="page-title">{frontmatter.title}</h1>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: html,
+        }}
+      />
+
+      <div>
+        {data.allMarkdownRemark.edges.slice(1).map((edge, index) => {
+          if (index = 1)
+            return (
+              <div data-sal="fade" key={`cv-${index}`}>
+                <CvArticle edge={edge} />
+              </div>
+            )
+        })}
+      </div>
+    </>
+  )
+}
+
+const Cv = ({ data }) => (
+  <Layout>
+    <Helmet bodyAttributes={{ class: "cv-page" }}>
+      <title>CV Page</title>
+    </Helmet>
+    <main>
+      <div className="container">
+        <div>{md({ data })}</div>
+      </div>
+    </main>
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/cv/" } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          html
+          frontmatter {
+            date
+            title
+            role
+            company
+            dates
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Cv
---
+++ .history/src/pages/cv/index_20210112174054.js
@@ -0,0 +1,75 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { graphql } from "gatsby"
+import Layout from "../../components/Layout"
+import CvArticle from "../../components/CvArticle"
+
+const md = ({ data }) => {
+  const { frontmatter, html } = data.allMarkdownRemark.edges[0].node
+
+  return (
+    <>
+      <h1 className="page-title">{frontmatter.title}</h1>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: html,
+        }}
+      />
+
+      <div>
+        {data.allMarkdownRemark.edges.slice(1).map((edge, index) => {
+          if ((index = 1)) {
+            return (
+              <div key={`cv-${index}`}>
+                <CvArticle edge={edge} />
+              </div>
+            )
+          } else {
+            return (
+              <div data-sal="fade" key={`cv-${index}`}>
+                <CvArticle edge={edge} />
+              </div>
+            )
+          }
+        })}
+      </div>
+    </>
+  )
+}
+
+const Cv = ({ data }) => (
+  <Layout>
+    <Helmet bodyAttributes={{ class: "cv-page" }}>
+      <title>CV Page</title>
+    </Helmet>
+    <main>
+      <div className="container">
+        <div>{md({ data })}</div>
+      </div>
+    </main>
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/cv/" } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          html
+          frontmatter {
+            date
+            title
+            role
+            company
+            dates
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Cv
